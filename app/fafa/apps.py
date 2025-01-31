56a944dbc20d04d074ed2f33aa629f56cdc9283a
luizmipc <luizmipc@gmail.com>
2025-01-31 13:48:31 -0300

    feat: Docker setted for production and development

---
+++ app/fafa/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FafaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fafa"
---
+++ app/jjas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JjasConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "jjas"
