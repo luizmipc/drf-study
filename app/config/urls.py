f56ed8b81a3ec507d5e24a44302c762849d06405
luizmipc <luizmipc@gmail.com>
2025-02-02 22:37:09 -0300

    browsable api not good

--- app/config/urls.py
+++ app/config/urls.py
@@ -18,11 +18,9 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 
-from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This is a comment
 echo "running!"
-uv run app/manage.py makemigrations
-uv run app/manage.py migrate
-uv run app/manage.py runserver 0.0.0.0:8000 # in dev
+cd app
+uv run manage.py makemigrations
+uv run manage.py migrate
+uv run manage.py runserver 0.0.0.0:8000 # in dev
 #uv run manage.py runserver 0.0.0.0:8000 # in prod
